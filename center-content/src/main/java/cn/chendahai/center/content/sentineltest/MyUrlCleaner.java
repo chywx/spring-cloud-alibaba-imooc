f8c9d261289416d53ed1c145191a2fa9b639019d
dahai <1559843332@qq.com>
2020-12-12 15:23:29 +0800

    赔率计算

--- center-content/src/main/java/cn/chendahai/center/content/sentineltest/MyUrlCleaner.java
+++ center-content/src/main/java/cn/chendahai/center/content/sentineltest/MyUrlCleaner.java
@@ -1,15 +1,17 @@
 package cn.chendahai.center.content.sentineltest;
 
 import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.UrlCleaner;
+import java.math.BigDecimal;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.math.NumberUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
-@Component
+//@Component
 @Slf4j
 public class MyUrlCleaner implements UrlCleaner {
+
     @Override
     public String clean(String originUrl) {
         // 让 /shares/1 与 /shares/2 的返回值相同
@@ -26,5 +28,16 @@ public class MyUrlCleaner implements UrlCleaner {
             })
             .reduce((a, b) -> a + "/" + b)
             .orElse("");
+    }
+
+    // 计算返奖
+    public static void main(String[] args) {
+        BigDecimal amount = new BigDecimal("20.00");
+        BigDecimal odds = new BigDecimal("1.850");
+
+        BigDecimal winAmount = amount.multiply(odds);
+        BigDecimal rewardAmount = winAmount.subtract(winAmount.subtract(amount).multiply(new BigDecimal("0.20"))).setScale(2, BigDecimal.ROUND_FLOOR);
+        System.out.println(rewardAmount);
+
     }
 }
